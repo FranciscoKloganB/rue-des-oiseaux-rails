f3944bada3d93db8f247ddfb795ab473236b82bc
Francisco Barros <francisco.debarros@paydock.com>
2025-11-19 23:10:33 +0000

    refactor: update routes to set root path and reorder health check route

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Returns 200 when the application boots with no exceptions, otherwise 500.
-  get "up" => "rails/health#show", as: :rails_health_check
-
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  root "guests#list"
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Returns 200 when the application boots with no exceptions, otherwise 500.
+  get "up" => "rails/health#show", as: :rails_health_check
+
   post "/guests", to: "guests#create"
 
   get "/guests", to: "guests#list"
