d672f5e611eaf12517a1919927ff6819f8d7caa2
Francisco Barros <francisco.debarros@paydock.com>
2025-11-27 16:03:54 +0000

    chore: create PagesController

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  root "guests#list"
+  root to: "pages#home"
 
   # Returns 200 when the application boots with no exceptions, otherwise 500.
   get "up" => "rails/health#show", as: :rails_health_check
