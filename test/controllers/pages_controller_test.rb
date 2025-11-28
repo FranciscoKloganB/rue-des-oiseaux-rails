74e51043da390caae63fed489298e54baee7ee2b
Francisco Barros <francisco.debarros@paydock.com>
2025-11-28 12:12:02 +0000

    test: add system and controller tests for home page elements and navigation

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -5,4 +5,43 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     get root_url
     assert_response :success
   end
+
+  test "home page contains main heading" do
+    get root_url
+    assert_select "h1", text: "Data to enrich your business"
+  end
+
+  test "home page contains description text" do
+    get root_url
+    assert_select "p", text: /Anim aute id magna aliqua/
+  end
+
+  test "home page contains navbar with logo" do
+    get root_url
+    assert_select "header div.navbar"
+    assert_select "img[alt='Rue Des Oiseaux']"
+  end
+
+  test "home page contains hamburger menu button" do
+    get root_url
+    assert_select "label[for='mobile-drawer']"
+  end
+
+  test "home page contains drawer sidebar" do
+    get root_url
+    assert_select "input#mobile-drawer[type='checkbox']"
+    assert_select ".drawer-side"
+  end
+
+  test "sidebar contains navigation links" do
+    get root_url
+    assert_select ".drawer-side ul.menu li a", text: "Register"
+    assert_select ".drawer-side ul.menu li a", text: "Login"
+  end
+
+  test "home page contains hero image" do
+    get root_url
+    assert_select "picture"
+    assert_select "img[alt='Rue Des Oiseaux']", count: 2 # logo + hero image
+  end
 end
---
+++ test/system/home_page_test.rb
@@ -0,0 +1,109 @@
+require "application_system_test_case"
+
+class HomePageTest < ApplicationSystemTestCase
+  test "visiting the home page" do
+    visit root_url
+
+    assert_selector "h1", text: "Data to enrich your business"
+    assert_text "Anim aute id magna aliqua ad ad non deserunt sunt"
+  end
+
+  test "home page displays logo" do
+    visit root_url
+
+    assert_selector "img[alt='Rue Des Oiseaux']"
+  end
+
+  test "home page displays hero image" do
+    visit root_url
+
+    assert_selector "picture"
+    # Check for responsive image sources (they are hidden by default in picture elements)
+    assert_selector "source[media='(min-width: 1280px)']", visible: :all
+    assert_selector "source[media='(min-width: 1024px)']", visible: :all
+    assert_selector "source[media='(min-width: 320px)']", visible: :all
+  end
+
+  test "hamburger menu button is visible" do
+    visit root_url
+
+    assert_selector "label[for='mobile-drawer']"
+    assert_selector "label[for='mobile-drawer'] svg"
+  end
+
+  test "drawer sidebar is hidden by default" do
+    visit root_url
+
+    # Drawer checkbox should be unchecked by default
+    assert_selector "input#mobile-drawer[type='checkbox']:not(:checked)", visible: :hidden
+  end
+
+  test "clicking hamburger menu opens drawer sidebar" do
+    visit root_url
+
+    # Initially, sidebar menu items should not be visible
+    refute_selector ".drawer-side ul.menu li a", text: "Register", visible: :visible
+
+    # Wait for drawer to open and check if menu items are visible
+    find("label[for='mobile-drawer']").click
+    assert_selector ".drawer-side ul.menu li a", text: "Register", visible: :visible
+    assert_selector ".drawer-side ul.menu li a", text: "Login", visible: :visible
+  end
+
+  test "drawer sidebar contains correct navigation links" do
+    visit root_url
+
+    # Open the drawer
+    find("label[for='mobile-drawer']").click
+
+    # Check navigation links
+    within ".drawer-side ul.menu" do
+      assert_selector "li a", text: "Register"
+      assert_selector "li a", text: "Login"
+    end
+  end
+
+  test "clicking drawer overlay closes the drawer" do
+    visit root_url
+
+    # Open the drawer
+    find("label[for='mobile-drawer']").click
+    assert_selector ".drawer-side ul.menu li a", text: "Register", visible: :visible
+
+    # Click the overlay to close
+    find(".drawer-overlay").click
+
+    # Menu items should no longer be visible
+    refute_selector ".drawer-side ul.menu li a", text: "Register", visible: :visible
+  end
+
+  test "drawer menu items are clickable" do
+    visit root_url
+
+    # Open the drawer
+    find("label[for='mobile-drawer']").click
+
+    # Verify menu items are present and clickable
+    within ".drawer-side ul.menu" do
+      assert_selector "li a[href='#']", text: "Register"
+      assert_selector "li a[href='#']", text: "Login"
+    end
+  end
+
+  test "navbar has proper z-index positioning" do
+    visit root_url
+
+    # Navbar should have z-50 class for proper stacking
+    assert_selector "header.z-50"
+  end
+
+  test "drawer has proper z-index positioning" do
+    visit root_url
+
+    # Open drawer to check its styling
+    find("label[for='mobile-drawer']").click
+
+    # Drawer side should have z-50 class
+    assert_selector ".drawer-side.z-50"
+  end
+end
